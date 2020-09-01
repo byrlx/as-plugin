c43b8bf79655fce179d0da57e76d40bb4fd149a0
GitHub Action <action@github.com>
2020-09-01 06:40:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/byrlx/asplugin/MyBundle.kt
+++ src/main/kotlin/com/github/byrlx/asplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.byrlx.asplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/byrlx/asplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/byrlx/asplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.byrlx.asplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.byrlx.asplugin.MyBundle
 
 class MyApplicationService {
 
